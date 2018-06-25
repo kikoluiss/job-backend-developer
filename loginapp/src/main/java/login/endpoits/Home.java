8e53e3325c76d8cd54103b6c705603dfaf65a15c
Kiko Santos <kikoluiss@gmail.com>
2018-06-25 20:53:20 -0300

    Adding resource rest endpoint

---
+++ loginapp/src/main/java/login/endpoits/Home.java
@@ -0,0 +1,13 @@
+package login.endpoits;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Home page according to user's profile";
+    }
+}
